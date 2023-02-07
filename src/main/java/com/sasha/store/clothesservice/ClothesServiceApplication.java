49ee3ef4e7fc08916cbb22b5bd53fffde0b4e7e0
Juan Castro <juanpcb1@gmail.com>
2023-02-06 21:29:42 -0500

    Base project

---
+++ src/main/java/com/sasha/store/clothesservice/ClothesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sasha.store.clothesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sasha/store/clothesservice/ClothesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasha.store.clothesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
