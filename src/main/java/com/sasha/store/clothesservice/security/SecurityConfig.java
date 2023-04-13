fc8f675d859dec4157aca9b05e2e4030fafc93bb
juanpablocastro96 <48337267+juanpablocastro96@users.noreply.github.com>
2023-04-12 22:11:45 -0500

    Feature/testing GitHub actions (#2)
    * Testing GitHub Actions
    
    * Testing GitHub Actions with bash file(system file)
    
    * Adding Dockerfile and testing with GitHub Actions
    
    * Refactoring and adding actuator for monitoring microservice
    
    ---------
    
    Co-authored-by: Juan Castro <v-jcastro3@expediagroup.com>

--- src/main/java/com/sasha/store/clothesservice/security/SecurityConfig.java
+++ src/main/java/com/sasha/store/clothesservice/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/register")
+                .requestMatchers("/api/v1/auth/register", "/actuator/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
